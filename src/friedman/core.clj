bee9fb6231f1ffa57ccabd1c64f056fe52131ac3
methylene <theairspeedindicator@yahoo.com>
2013-08-17 09:41:08 +0200

    Print all 2-symbol *-sequences of length 11

--- src/friedman/core.clj
+++ src/friedman/core.clj
@@ -1,13 +1,19 @@
 (ns friedman.core
   (:require [friedman.n :refer [is-not-* slice]]
+            [friedman.lab :refer [all-strings-of-length]]
             [clojure.tools.logging :refer [infof]]
             [clojure.string :refer [join]])
   (:gen-class))
 
 (defn -main
   [& args]
-  (let [x '(1 2 2 2 1 1 1 1 1 1 1)] ; try variations
-    (if-let [fail-info (is-not-* x)]
+  (doseq [x (all-strings-of-length [0 1] 11)]
+    (if (not (is-not-* x))
+      (printf "Sequence x = (%s) has property *.%n" (join " " x))))
+  )
+
+(defn print-sequence-info [x]
+  (if-let [fail-info (is-not-* x)]
       (let [i0 (first fail-info)
             i1 (* 2 (inc (first fail-info)))
             j0 (second fail-info)
@@ -20,4 +26,4 @@
                     "z = x[%d], ..., x[%d] = '(%s)%n"
                     "(0-based indexes)")
                x i0 (dec i1) (join " " low-seq) j0 (dec j1) (join " " high-seq)))
-      (infof "Sequence x = %s has property *." x))))
+      (infof "Sequence x = %s has property *." x)))
